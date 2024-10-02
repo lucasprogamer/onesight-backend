82d42175247351b095ac96358c8e6f0b6da0e98b
Lucas | Alves <lucasouzaweb@hotmail.com>
2024-10-02 11:32:04 -0300

    feat: add migrations

---
+++ src/Entity/Schedule.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ScheduleRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ScheduleRepository::class)]
+class Schedule
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $datetime = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $event = null;
+
+    #[ORM\Column]
+    private ?int $priority = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDatetime(): ?\DateTimeInterface
+    {
+        return $this->datetime;
+    }
+
+    public function setDatetime(\DateTimeInterface $datetime): static
+    {
+        $this->datetime = $datetime;
+
+        return $this;
+    }
+
+    public function getEvent(): ?string
+    {
+        return $this->event;
+    }
+
+    public function setEvent(string $event): static
+    {
+        $this->event = $event;
+
+        return $this;
+    }
+
+    public function getPriority(): ?int
+    {
+        return $this->priority;
+    }
+
+    public function setPriority(int $priority): static
+    {
+        $this->priority = $priority;
+
+        return $this;
+    }
+
+    public function toArray(): array
+    {
+        return [
+            'id' => $this->getId(),
+            'datetime' => $this->getDatetime()->format('Y-m-d H:i:s'),
+            'event' => $this->getEvent(),
+            'priority' => $this->getPriority(),
+        ];
+    }
+}
